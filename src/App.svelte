6de327ecae224e55f804b7868823c9e9edcd4266
Jaimin Prajapati <enthralall@gmail.com>
2022-05-24 00:45:33 +0530

    Creating Feedback List, Feedback Item, Card components and setup in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,27 @@
 <script>
+    import FeedbackList from "./components/FeedbackList.svelte";
+
+    let feedback = [
+        {
+            id: 1,
+            rating: 10,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae."
+        },
+        {
+            id: 2,
+            rating: 9,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae."
+        },
+        {
+            id: 3,
+            rating: 8,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae."
+        }
+    ];
 </script>
 
 <main>
-	<h1>Hello</h1>
+    <FeedbackList {feedback} />
 </main>
 
 <style>
