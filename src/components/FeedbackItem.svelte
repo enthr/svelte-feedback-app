413a770b8c2ef1fc55c40299e38e091ec54e4abc
Jaimin Prajapati <enthralall@gmail.com>
2022-05-24 01:02:36 +0530

    Building The Card Component and Creating Custom Event and Dispatch it to delte data

--- src/components/FeedbackItem.svelte
+++ src/components/FeedbackItem.svelte
@@ -1,8 +1,43 @@
 <script>
+    import { createEventDispatcher } from "svelte";
     import Card from "./Card.svelte";
     export let item;
+
+    const handleDelete = (itemId) => {
+        
+    } 
 </script>
 
 <Card>
-    Hello
+    <div class="num-display">
+        {item.rating}
+    </div>
+    <button class="close" on:click={() => handleDelete(item.id)}>X</button>
+    <p class="text-display">{item.text}</p>
 </Card>
+
+<style>
+    .num-display {
+        position: absolute;
+        top: -10px;
+        left: -10px;
+        width: 50px;
+        height: 50px;
+        background-color: #ff6a95;
+        color: #ffffff;
+        border: 1px #eeeeee solid;
+        border-radius: 50%;
+        padding: 10px;
+        text-align: center;
+        font-size: 19px;
+    }
+
+    .close {
+        position: absolute;
+        top: 10px;
+        right: 20px;
+        cursor: pointer;
+        background: none;
+        border: none;
+    }
+</style>
